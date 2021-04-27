2b6e1e7bfbf7a3d0480cf3be16b1a08bed5e9599
claudio <cgalvezv@gmail.com>
2021-04-27 17:53:21 -0400

    intento de agregar articulo

---
+++ scripts/news.js
@@ -0,0 +1,20 @@
+/* var titulo =  document.getElementById("title").value;
+var categoria = document.getElementById("category").value;
+var cuerpo = document.getElementById("article").value;
+alert(titulo);
+    alert(categoria);
+    alert(cuerpo);
+var html =  "<article class='col-xs-12 col-sm-12 col-md-6 col-lg-4'><div class='titulo-noticia'>+titulo+</div><div class='categoria-noticia'>+categoria+</div><div class='texto-noticia'>+cuerpo"
+document.getElementById("news").innerhtml = html  */
+
+function myFunction() {
+    var titulo =  document.getElementById("title").value;
+    var categoria = document.getElementById("category").value;
+    var cuerpo = document.getElementById("article").value;
+    alert(titulo);
+    alert(categoria);
+    alert(cuerpo);
+    var html = "<article class='col-xs-12 col-sm-12 col-md-6 col-lg-4' id='news'><div class='titulo-noticia'>+titulo+</div><div class='categoria-noticia'>+categoria+</div><div class='texto-noticia'>+cuerpo";
+    alert(html);
+    document.body.appendChild(html);
+}
