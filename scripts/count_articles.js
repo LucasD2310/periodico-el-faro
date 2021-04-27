ad5b83e94279588af57942b90319a9b73ad015b1
Lucas <diazc.lucas@gmail.com>
2021-04-27 18:54:00 -0400

    limpieza y agregado de newsletter a demas secciones

--- scripts/count_articles.js
+++ scripts/count_articles.js
@@ -1,6 +1,2 @@
 const articles = document.getElementsByClassName('col-xs-12 col-sm-12 col-md-6 col-lg-4').length;
-<<<<<<< HEAD
 document.getElementById('articulo').innerHTML = `<p class="count">Articulos: ${articles}</p>`
-=======
-document.getElementById('articulo').innerHTML = `<p class="count">Artículos: ${articles}</p>`
->>>>>>> 5bc3b9e782dc9fe28304f16d937197a0791a8e38
