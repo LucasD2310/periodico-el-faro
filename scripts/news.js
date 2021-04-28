4382771927383528c05cdf04c20023b41bf0ce66
claudio <cgalvezv@gmail.com>
2021-04-27 20:53:15 -0400

    intento de agregar articulo

--- scripts/news.js
+++ scripts/news.js
@@ -11,10 +11,7 @@ function myFunction() {
     var titulo =  document.getElementById("title").value;
     var categoria = document.getElementById("category").value;
     var cuerpo = document.getElementById("article").value;
-    alert(titulo);
-    alert(categoria);
-    alert(cuerpo);
     var html = "<article class='col-xs-12 col-sm-12 col-md-6 col-lg-4' id='news'><div class='titulo-noticia'>+titulo+</div><div class='categoria-noticia'>+categoria+</div><div class='texto-noticia'>+cuerpo";
     alert(html);
-    document.body.appendChild(html);
+    document.getElementById("new").appendChild(html);
 }
